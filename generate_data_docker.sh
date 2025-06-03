6100921543ad6b3c9ae83b60577c4dd46916db43
Wade Waldron <wwaldron@confluent.io>
2025-06-03 08:45:11 -0600

    Pinned a specific version of JR in the docker container. Fixed the system dir flag for that version.

--- generate_data_docker.sh
+++ generate_data_docker.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run -v ./jr:/jr -it jrndio/jr:latest sh -c 'jr --system_dir /jr/ emitter run gaming --log_level debug'
+docker run -v ./jr:/jr -it jrndio/jr:v0.4.0 sh -c 'jr --jr_system_dir /jr/ emitter run gaming --log_level debug'
